fe7b1c822f22ad9a6ed0b32bfc0fce474798e372
umerkang66 <umerkang66@gmail.com>
2021-07-26 16:04:21 +0500

    improved the typedOutStrings easy solution a little bit

--- Strings/[easy]typedOutStrings.ts
+++ Strings/[easy]typedOutStrings.ts
@@ -4,7 +4,7 @@ const str2 = 'abzz##d';
 // CODING OUR BRUTE FORCE SOLUTION
 const strBackspacedBruteForce = (str: string): string => {
   const strArr = str.split('');
-  let strArrBackspaced = [];
+  let strArrBackspaced: string[] = [];
   for (let i = 0; i < strArr.length; i++) {
     if (strArr[i] !== '#') strArrBackspaced.push(strArr[i]);
     else strArrBackspaced.pop();
@@ -13,10 +13,10 @@ const strBackspacedBruteForce = (str: string): string => {
   return strArrBackspaced.join('');
 };
 
-const isTypedOutStringsEqualBruteForce = function (
+const isTypedOutStringsEqualBruteForce = (
   str1: string,
   str2: string
-): boolean {
+): boolean => {
   const str1Backspaced = strBackspacedBruteForce(str1);
   const str2Backspaced = strBackspacedBruteForce(str2);
 
@@ -26,40 +26,39 @@ const isTypedOutStringsEqualBruteForce = function (
 
 isTypedOutStringsEqualBruteForce(str1, str2);
 
-// CODING OUR OPTIMIZE SOLUTION: THERE IS NO NEED TO OPTIMIZE THE TIME COMPLEXITY BECAUSE IT IS ALREADY O(n), BUT WE NEED TO OPTIMIZE THE SPACE COMPLEXITY, BY REMOVING THE USE OF ARRAY BECAUSE WE ARE NOT RETURNING ARRAY, WE ARE SIMPLY JUST RETURNING TRUE, OR FALSE
+// EXPLANATION: We have written an if else block, in the if block we only deals with the case if there are hashes in strings and move the pointers accordingly, if there is 1 hashe the pointer will move 2 times, if there are 2 hashes the pointer will move 4 times. Then after the if block we deals with checking if the two strings are equal and we also move the normal pointers backwards without the influence of hashes
 const isTypedOutStrings = (str1: string, str2: string): boolean => {
   let pointer1 = str1.length - 1;
   let pointer2 = str2.length - 1;
+
   while (pointer1 >= 0 || pointer2 >= 0) {
     if (str1[pointer1] === '#' || str2[pointer2] === '#') {
       if (str1[pointer1] === '#') {
-        let backcount = 2;
+        let backCount = 2;
 
-        while (backcount > 0) {
+        while (backCount > 0) {
           pointer1--;
-          backcount--;
+          backCount--;
 
-          if (str1[pointer1] === '#') backcount += 2;
+          if (str1[pointer1] === '#') backCount += 2;
         }
       }
 
       if (str2[pointer2] === '#') {
-        let backcount = 2;
+        let backCount = 2;
 
-        while (backcount > 0) {
+        while (backCount > 0) {
           pointer2--;
-          backcount--;
+          backCount--;
 
-          if (str2[pointer2] === '#') backcount += 2;
+          if (str2[pointer2] === '#') backCount += 2;
         }
       }
-    } else {
-      if (str1[pointer1] !== str2[pointer2]) return false;
-      else {
-        pointer1--;
-        pointer2--;
-      }
     }
+
+    if (str1[pointer1] !== str2[pointer2]) return false;
+    pointer1--;
+    pointer2--;
   }
 
   return true;
