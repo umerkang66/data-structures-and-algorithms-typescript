65b9f76aa263a66d65f8949b8abf5d28c547e0c5
umerkang66 <umerkang66@gmail.com>
2021-07-26 09:16:39 +0500

    added typed annotation in the index.ts file

--- LinkedList/index.ts
+++ LinkedList/index.ts
@@ -1,5 +1,7 @@
 import LinkedList from './LinkedList';
 import DoublyLinkedList from './DoublyLinkedList';
 
-const linkedList = new LinkedList(5);
-const doublyLinkedList = new DoublyLinkedList(5);
+const linkedList = new LinkedList<number>(5);
+const doublyLinkedList = new DoublyLinkedList<number>(5);
+
+console.log(linkedList, doublyLinkedList);
