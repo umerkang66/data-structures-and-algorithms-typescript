babb610a43a078cef25adb820c8a8fd8b3171680
umerkang66 <umerkang66@gmail.com>
2021-08-13 23:30:22 +0500

    solved the minimumBracketToRemove medium stack question

--- StacksQueues/minimumBracketsToRemove.ts
+++ StacksQueues/minimumBracketsToRemove.ts
@@ -1,5 +1,21 @@
-const str = 'a)bc(d)';
+const str = 'a)b(c)d';
 
-const minimumBracketsToRemove = (str: string): string => {
-  return '';
+const minimumBracketsToRemove = (str: string) => {
+  const res = str.split('');
+  const stack = [];
+
+  for (let i = 0; i < res.length; i++) {
+    if (res[i] === '(') stack.push(i);
+    else if (res[i] === ')' && stack.length) stack.pop();
+    else if (res[i] === ')') res[i] = '';
+  }
+
+  while (stack.length) {
+    const curIdx = stack.pop();
+    if (curIdx) res[curIdx] = '';
+  }
+
+  return res.join('');
 };
+
+minimumBracketsToRemove(str);
