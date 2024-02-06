5dfde9a654b5827589d1f9773e9c46cf7fe3f771
Muhammad Umer <umerkang66@gmail.com>
2024-02-06 19:52:45 +0500

    add quick sort

--- colt-steele-dsa/src/sorting/__test__/sort.test.ts
+++ colt-steele-dsa/src/sorting/__test__/sort.test.ts
@@ -1,6 +1,7 @@
 import { bubbleSort } from '../bubble-sort';
 import { insertionSort } from '../insertion-sort';
 import { mergeSort } from '../merge-sort';
+import { quickSort } from '../quick-sort';
 
 it('sorts the array using bubble sort', () => {
   const nums = [7, 2, 12, 8, 16, 9];
@@ -17,5 +18,11 @@ it('sorts the array using insertion sort', () => {
 it('sorts the array using merge sort', () => {
   const nums = [7, 2, 12, 8, 16, 9];
   mergeSort(nums, 0, nums.length - 1);
+  expect(nums).toEqual([2, 7, 8, 9, 12, 16]);
+});
+
+it('sorts the array using quick sort', () => {
+  const nums = [7, 2, 12, 8, 16, 9];
+  quickSort(nums, 0, nums.length - 1);
   expect(nums).toEqual([2, 7, 8, 9, 12, 16]);
 });
--- colt-steele-dsa/src/sorting/index.ts
+++ colt-steele-dsa/src/sorting/index.ts
@@ -1,5 +1,5 @@
-import { mergeSort } from './merge-sort';
+import { quickSort } from './quick-sort';
 
 const nums = [7, 2, 12, 8, 16, 9];
-mergeSort(nums, 0, nums.length - 1);
+quickSort(nums, 0, nums.length - 1);
 console.log(nums);
